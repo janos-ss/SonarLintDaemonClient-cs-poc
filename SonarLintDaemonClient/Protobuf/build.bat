aa9b1218753c1bcad57c79d76007b6ab5f94ca58
Janos Gyerik <janos.gyerik+windows@sonarsource.com>
2017-04-13 15:47:30 +0200

    Add scripts to generate protobuf

---
+++ SonarLintDaemonClient/Protobuf/build.bat
@@ -0,0 +1,1 @@
+"..\..\..\packages\Grpc.Tools.1.2.2\tools\windows_x64\protoc.exe" -I=. --csharp_out . --grpc_out . ./sonarlint-daemon.proto --plugin=protoc-gen-grpc=../../../packages/Grpc.Tools.1.2.2/tools/windows_x64/grpc_csharp_plugin.exe
