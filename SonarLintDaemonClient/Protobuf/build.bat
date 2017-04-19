f4a2b781b2e5872e5339f09771a26b7b8b3c31d2
Janos Gyerik <janos.gyerik+windows@sonarsource.com>
2017-04-19 09:55:16 +0200

    Extract repeated tools path to variable

--- SonarLintDaemonClient/Protobuf/build.bat
+++ SonarLintDaemonClient/Protobuf/build.bat
@@ -1,1 +1,2 @@
-"..\..\packages\Grpc.Tools.1.2.2\tools\windows_x64\protoc.exe" -I=. --csharp_out . --grpc_out . ./sonarlint-daemon.proto --plugin=protoc-gen-grpc=../../packages/Grpc.Tools.1.2.2/tools/windows_x64/grpc_csharp_plugin.exe
+set toolspath=..\..\packages\Grpc.Tools.1.2.2\tools\windows_x64
+%toolspath%\protoc.exe -I=. --csharp_out . --grpc_out . sonarlint-daemon.proto --plugin=protoc-gen-grpc=%toolspath%\grpc_csharp_plugin.exe
